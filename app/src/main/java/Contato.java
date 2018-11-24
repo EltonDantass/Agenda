0e08ef47dc7ae78eaeb5d09917c32dbb0b9059e0
Elton Dantas <mr.eltondantas@gmail.com>
2018-11-23 21:39:23 -0300

    Initial commit

---
+++ app/src/main/java/Contato.java
@@ -0,0 +1,61 @@
+import android.content.ContentValues;
+
+public class Contato {
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Integer getPromocoes() {
+        return promocoes;
+    }
+
+    public void setPromocoes(Integer promocoes) {
+        this.promocoes = promocoes;
+    }
+
+    String nome, email, telefone, nascimento,sexo;
+    Integer promocoes;
+
+
+    public ContentValues getContentValues(){
+        ContentValues cv = new ContentValues();
+        cv.put("nome",this.nome);
+    }
+}
---
+++ app/src/main/java/DataBase.java
@@ -0,0 +1,58 @@
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class DataBase extends SQLiteOpenHelper {
+
+        private static final String NOME_BANCO = "contatos.db";
+        private static final int VERSAO_BANCO = 1;
+        private Context context;
+        private SQLiteDatabase dbInstancia = null;
+        DB(Context context){
+            super(context,NOME_BANCO,null,VERSAO_BANCO);
+            this.context = context;
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE contatos(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " nome TEXT NOT NULL, email TEXT, telefone TEXT, nascimento TEXT, sexo TEXT, promocoes INTEGER)");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+
+        public void abrirDB() throws SQLException {
+            if(this.dbInstancia == null){
+                this.dbInstancia = this.getWritableDatabase();
+            }
+        }
+
+        public void fecharDB() throws SQLException {
+            if(this.dbInstancia != null){
+                if(this.dbInstancia.isOpen()){
+                    this.dbInstancia.close();
+                }
+            }
+        }
+
+        public void salvarContato(Contato contato) throws SQLException{
+            abrirDB();
+            dbInstancia.insert("contatos",null,contato.getContentValues());
+            Toast.makeText(context,contato.getNome()+"cadastrado",Toast.LENGTH_LONG).show();
+            fecharDB();
+        }
+
+
+/*
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS contatos");
+        onCreate(db);
+    } */
+    }
+
